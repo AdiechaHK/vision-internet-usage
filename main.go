fea929d462ba72cd4b61f7604662af495c1a247a
Harikrushna <harikrushna@Harikrushnas-MacBook-Air.local>
2021-11-28 14:10:32 +0530

    added log to verify if server is running on currect port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -13,5 +14,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", hello)
+	fmt.Println("Listening on port: " + port)
 	http.ListenAndServe(":"+port, nil)
 }
