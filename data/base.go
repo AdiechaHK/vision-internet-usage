7f5a258197eeb0c47e800265e4297d0117b309e3
Harikrushna V Adiecha <adiechahari@gmail.com>
2021-12-02 15:08:29 +0530

    complete basic functionality

---
+++ data/base.go
@@ -0,0 +1,132 @@
+package data
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Record struct {
+	Used       int   `json:"used"`
+	Available  int   `json:"available"`
+	RecordedAt int64 `json:"recorded_at"`
+}
+
+type DataCollection struct {
+	Records []Record `json:"records"`
+}
+
+type GistFile struct {
+	Name    string `json:"filename"`
+	Type    string `json:"type"`
+	Lang    string `json:"language"`
+	Content string `json:"content"`
+}
+
+type Gist struct {
+	Id    string              `json:"id"`
+	Files map[string]GistFile `json:"files"`
+}
+
+func apiCall(method string, uri string, body io.Reader) []byte {
+	username := os.Getenv("GITHUB_USERNAME")
+	token := os.Getenv("GITHUB_TOKEN")
+	url := "https://api.github.com" + uri
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		panic(err)
+	}
+	req.SetBasicAuth(username, token)
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func getGist() *Gist {
+	gistId := os.Getenv("GIST_ID")
+	res := apiCall("GET", "/gists/"+gistId, nil)
+	gist := Gist{}
+	json.Unmarshal(res, &gist)
+	return &gist
+}
+
+func saveGist(g *Gist) {
+	data, err := json.Marshal(g)
+	if err != nil {
+		panic(err)
+	}
+	apiCall("PATCH", "/gists/"+g.Id, bytes.NewReader(data))
+}
+
+func latestData(d *DataCollection) (int, int) {
+	fmt.Println("Checking latest")
+	max := int64(0)
+	var lu, lr int
+
+	for _, item := range d.Records {
+		if item.RecordedAt > max {
+			max = item.RecordedAt
+			lu = item.Used
+			lr = item.Available
+		}
+	}
+
+	return lu, lr
+}
+
+func addData(used int, remain int, gist *Gist) {
+
+	fileName := os.Getenv("FILENAME")
+
+	for _, f := range gist.Files {
+		dc := DataCollection{}
+		if f.Name == fileName {
+			err := json.Unmarshal([]byte(f.Content), &dc)
+			if err != nil {
+				panic(err)
+			}
+
+			if len(dc.Records) > 0 {
+				lu, lr := latestData(&dc)
+				if used == lu && remain == lr {
+					fmt.Println("No need to update")
+					continue
+				}
+			}
+			fmt.Println("Updating data")
+
+			rec := Record{}
+			rec.Available = remain
+			rec.Used = used
+			rec.RecordedAt = time.Now().UnixMilli()
+			dc.Records = append(dc.Records, rec)
+
+			cnt, err := json.Marshal(dc)
+			if err != nil {
+				panic(err)
+			}
+			f.Content = string(cnt)
+			gist.Files[fileName] = f
+		}
+	}
+
+}
+
+func StoreData(used int, remain int) {
+	gist := getGist()
+	addData(used, remain, gist)
+	// saveGist(gist)
+}
--- schedule/scrap.go
+++ schedule/scrap.go
@@ -1,7 +1,100 @@
 package schedule
 
-import "fmt"
+import (
+	"fmt"
+	"hello-heroku/data"
+	"regexp"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+func makeLogin(url string, fdt map[string]string) {
+	c := colly.NewCollector()
+
+	loggedIn := false
+
+	c.OnHTML("#ContentPlaceHolder1_tbAdditonal_tpPlan_gdPlan", func(ele *colly.HTMLElement) {
+		remain := ele.ChildText("#ContentPlaceHolder1_tbAdditonal_tpPlan_gdPlan_lblRemain_0")
+		used := ele.ChildText("#ContentPlaceHolder1_tbAdditonal_tpPlan_gdPlan_lblUsed_0")
+		re := regexp.MustCompile(`^\d+`)
+
+		u, err := strconv.Atoi(re.FindString(used))
+		if err != nil {
+			panic(err)
+		}
+		r, err := strconv.Atoi(re.FindString(remain))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Used: " + used + " and Remain: " + remain)
+		data.StoreData(u, r)
+		if loggedIn {
+			fmt.Println("Logged in")
+		} else {
+			fmt.Println("Not logged in")
+		}
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+
+		if loggedIn {
+			fmt.Println("-------------------------PLAN PAGE-----------------------------------")
+			// fmt.Println("START ------------------------------------------------------------")
+			// fmt.Println(string(r.Body))
+
+			// op, err := os.Create("op.html")
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// defer op.Close()
+
+			// lineCount, err := op.Write(r.Body)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// fmt.Printf("wrote %d bytes\n", lineCount)
+			// fmt.Println("END ------------------------------------------------------------")
+		} else {
+			loggedIn = true
+			url = r.Request.AbsoluteURL("AccountViewPlan.aspx")
+			fmt.Println("Going to " + url)
+			c.Visit(url)
+		}
+
+	})
+	c.Post(url, fdt)
+}
 
 func TestFun() {
-	fmt.Println("Testing function")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("form", func(e *colly.HTMLElement) {
+
+		fdt := make(map[string]string)
+		e.ForEach("input", func(_ int, elem *colly.HTMLElement) {
+			name := elem.Attr("name")
+			value := elem.Attr("value")
+			fmt.Println(name, " >> ", value)
+			if name == "txtUserName" {
+				fdt[name] = "HARIKRUSHNA_KRUSHNANAGAR"
+			} else if name == "txtPassword" {
+				fdt[name] = "123456"
+			} else {
+				fdt[name] = value
+			}
+		})
+
+		url := e.Request.AbsoluteURL(e.Attr("action"))
+
+		fmt.Println(url, fdt)
+		makeLogin(url, fdt)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://3.7.153.175/vtpl/customer")
 }
